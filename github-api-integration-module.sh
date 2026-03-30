955c1707afd2a809f5dc6460e6b77fc3188949d2
dabadeabhishek35 <dabadeabhishek35@gmail.com>
2026-03-30 11:53:48 +0530

    chore: updated description

--- github-api-integration-module.sh
+++ github-api-integration-module.sh
@@ -9,7 +9,7 @@
 # Usage:
 #   Please provide your github token and rest api to the script as input
 #
-#
+#Something I wanted to add in description
 ################################
 
 if [ ${#@} -lt 2 ]; then
